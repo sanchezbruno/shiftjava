813e9a6179ad1d184f534874d63ea15242ebe001
Bruno Sanchez <sanchez.bruno@hotmail.com>
2023-02-23 21:21:34 -0300

    Adicionando Vetores

---
+++ src/Vetor.java
@@ -0,0 +1,17 @@
+
+public class Vetor {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {0,1,2,3,4,5,6,7,8,9};
+		
+		float[] notas = new float[10];
+		
+		for (int i = 0; i< arr.length; i++) {
+			System.out.println("Element at index " + i + ": " + arr[i]);
+		}
+		
+		
+	}
+
+}
