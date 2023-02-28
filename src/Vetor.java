af02cc3eedfdb8d6148e6153b6aaf6d65b9e68cb
Bruno Sanchez <sanchez.bruno@hotmail.com>
2023-02-28 19:28:11 -0300

    Teste de branch

--- src/Vetor.java
+++ src/Vetor.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Vetor {
 
@@ -5,13 +7,20 @@ public class Vetor {
 		
 		int[] arr = {0,1,2,3,4,5,6,7,8,9};
 		
-		float[] notas = new float[10];
+		//float[] notas = new float[10];
 		
 		for (int i = 0; i< arr.length; i++) {
 			System.out.println("Element at index " + i + ": " + arr[i]);
 		}
 		
 		
+		Integer[] numbers = {5, 45, 65 , 12, 50};
+		
+		Arrays.sort(numbers, Collections.reverseOrder());
+		
+		for (int num: numbers) {
+			System.out.println(num);
+		}
 	}
 
 }
